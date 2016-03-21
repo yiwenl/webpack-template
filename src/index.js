38c4993bff1529750208a1e29e858218e1c9d5d0
Wen <Wen@wen.local>
2016-03-21 14:37:13 +0000

    add alfrid

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
-console.log('init');
-
 import './global.scss';
+import alfrid , { Camera } from 'alfrid';
+
+let GL = alfrid.GL;
+
+window.addEventListener('load', _init);
 
 
-// let strShader = glslify('./basic.vert');
-let shader = require('./basic.vert')
-console.log('Shader : ', shader);
+let camera, bAxis, bDotPlane;
+
+function _init() {
+	let canvas = document.createElement("canvas");
+	document.body.appendChild(canvas);
+	canvas.className = 'Main-Canvas';
+	alfrid.GL.init(canvas);
+
+	camera = new alfrid.CameraPerspective();
+	camera.setPerspective(Math.PI/2, GL.aspectRatio, 1, 100);
+	let ctrl = new alfrid.OrbitalControl(camera, window, 5);
+
+	bAxis = new alfrid.BatchAxis();
+	bDotPlane = new alfrid.BatchDotsPlane();
+
+	alfrid.Scheduler.addEF(loop);
+}
+
+
+function loop() {
+	GL.clear(0, 0, 0, 0);
+	GL.setMatrices(camera);
+	bAxis.draw();
+	bDotPlane.draw();
+}
