e56e815fd0a3785611c20dc9acefbc976caf9e40
Wen <Wen@wen.local>
2016-04-01 10:54:40 +0100

    add assets-loader / dat-gui

--- src/js/app.js
+++ src/js/app.js
@@ -1,32 +1,60 @@
 import '../scss/global.scss';
 import alfrid , { Camera } from 'alfrid';
+import SceneApp from './SceneApp';
+import AssetsLoader from 'assets-loader';
+import dat from 'dat-gui';
 
-let GL = alfrid.GL;
+const GL = alfrid.GL;
+const assets = [];
 
-window.addEventListener('load', _init);
-
-let camera, bAxis, bDotPlane;
-
-function _init() {
-	let canvas = document.createElement("canvas");
-	document.body.appendChild(canvas);
-	canvas.className = 'Main-Canvas';
-	alfrid.GL.init(canvas);
-
-	camera = new alfrid.CameraPerspective();
-	camera.setPerspective(Math.PI/2, GL.aspectRatio, 1, 100);
-	let ctrl = new alfrid.OrbitalControl(camera, window, 5);
-
-	bAxis = new alfrid.BatchAxis();
-	bDotPlane = new alfrid.BatchDotsPlane();
-
-	alfrid.Scheduler.addEF(loop);
+if(document.body) {
+	_init();
+} else {
+	window.addEventListener('DOMContentLoaded', _init);	
 }
 
 
-function loop() {
-	GL.clear(0, 0, 0, 0);
-	GL.setMatrices(camera);
-	bAxis.draw();
-	bDotPlane.draw();
+function _init() {
+
+	if(assets.length > 0 ) {
+		document.body.classList.add('isLoading');
+
+		let loader = new AssetsLoader({
+			assets:assets
+		}).on('error', function(error) {
+			console.error(error);
+		}).on('progress', function(p) {
+			// console.log('Progress : ', p);
+			let loader = document.body.querySelector('.Loading-Bar');
+			if(loader) loader.style.width = (p * 100).toFixed(2) + '%';
+		}).on('complete', _onImageLoaded)
+		.start();	
+	} else {
+		_init3D();
+	}
+
+}
+
+
+function _onImageLoaded(o) {
+	console.log('Image Loaded : ', o);
+	document.body.classList.remove('isLoading');
+	window.assets = o;	
+
+	_init3D();
+}
+
+
+function _init3D() {
+	//	CREATE CANVAS
+	let canvas = document.createElement("canvas");
+	canvas.className = 'Main-Canvas';
+	document.body.appendChild(canvas);
+
+	GL.init(canvas);
+
+
+	let scene = new SceneApp();
+
+	window.gui = new dat.GUI({width:300});
 }
