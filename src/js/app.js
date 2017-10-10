71e85b920849b1515d9a9207d926c1c027dd2cb8
Yi-Wen Lin <bongiovi015@gmail.com>
2017-10-10 12:20:46 +0100

    Remove console logs

--- src/js/app.js
+++ src/js/app.js
@@ -42,15 +42,6 @@ function _init() {
 	} else {
 		_init3D();
 	}
-
-
-	// console.log('process.env.NODE_ENV', process.args);
-	for(let s in process) {
-		console.log(s);
-	}
-
-	console.log(process.env);
-	console.log(process.argv);
 }
 
 
