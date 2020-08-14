8880d5eddf7f6b79cb0482a5032acb8c070e889f
Yi-Wen Lin <bongiovi015@gmail.com>
2020-08-14 21:41:19 +0100

    update capture for windows

--- _project_templates/particlesMultiBuffer/src/js/utils/Capture.js
+++ _project_templates/particlesMultiBuffer/src/js/utils/Capture.js
@@ -1,30 +1,36 @@
 // Capture.js
 
-import { GL } from 'alfrid';
-import { saveImage } from './';
+import { GL } from "alfrid";
+import { saveImage } from "./";
 
 String.prototype.replaceAll = function(search, replacement) {
-    var target = this;
-    return target.replace(new RegExp(search, 'g'), replacement);
+  var target = this;
+  return target.replace(new RegExp(search, "g"), replacement);
 };
 
 const capture = () => {
-	window.addEventListener('keydown', (e) => {
-		if(e.keyCode === 83 && e.metaKey) {
-			e.preventDefault();
-			const date = new Date();
-			let strDate = 
-				`${date.getFullYear()}.` + 
-				`${date.getMonth() + 1}.` + 
-				`${date.getDate()}-` + 
-				`${date.getHours()}.` + 
-				`${date.getMinutes()}.` + 
-				`${date.getSeconds()}`;
-			
-			saveImage(GL.canvas, strDate);
-		}
-	});
-}
+  window.addEventListener("keydown", (e) => {
+    let toCapture = false;
+    if (window.navigator.userAgent.indexOf("Macintosh") > -1) {
+      toCapture = e.keyCode === 83 && e.metaKey;
+    } else {
+      toCapture = e.keyCode === 83 && e.ctrlKey;
+    }
 
+    if (toCapture) {
+      e.preventDefault();
+      const date = new Date();
+      let strDate =
+        `${date.getFullYear()}.` +
+        `${date.getMonth() + 1}.` +
+        `${date.getDate()}-` +
+        `${date.getHours()}.` +
+        `${date.getMinutes()}.` +
+        `${date.getSeconds()}`;
+
+      saveImage(GL.canvas, strDate);
+    }
+  });
+};
 
 export default capture();
